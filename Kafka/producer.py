d577f1ba8f566ef051260667b5631e9e4636c0c6
Mohamed TOUMI <103457375+polymoe@users.noreply.github.com>
2022-11-19 22:20:34 +0100

    Add files via upload

---
+++ Kafka/producer.py
@@ -0,0 +1,11 @@
+from kafka import KafkaProducer
+
+
+kafka_producer = KafkaProducer(
+  bootstrap_servers="localhost:9092"
+)
+
+for i in range(1, 30):
+    kafka_producer.send(topic="test", value=f"New message # {i}".encode("utf-8"))
+
+kafka_producer.flush()
